10622c3c5a086a48cd1e5cb217e873a09a6ed020
MichaelAguilar89 <mchlaguilar051@hotmail.es>
2024-05-30 22:14:48 -0600

    adding blazorserverApp to solution

--- ItemsRazorAuth/obj/Debug/net8.0/ItemsRazorAuth.AssemblyInfo.cs
+++ ItemsRazorAuth/obj/Debug/net8.0/ItemsRazorAuth.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ItemsRazorAuth")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b3de63d352864de2e522e0648ffb1485e66038c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+421688069ab4696fd39c6346f04453f44cc328a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ItemsRazorAuth")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ItemsRazorAuth")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
